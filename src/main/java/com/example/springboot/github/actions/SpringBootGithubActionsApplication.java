fef9ed8c6e3b576bb7ae41be6f333003703fbc86
908192 <908192@cognizant.com>
2024-05-22 15:45:19 +0530

    first coomit

---
+++ src/main/java/com/example/springboot/github/actions/SpringBootGithubActionsApplication.java
@@ -0,0 +1,22 @@
+package com.example.springboot.github.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootGithubActionsApplication {
+
+	@GetMapping("/welcome")
+	public String message(){
+		return "Welcome to Github Action CI CD Workflow!!";
+	}
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/github/actions/SpringBootGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.github.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
